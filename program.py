953203d27fa152706f957e0c9b4818f1a210bffc
sbenf999 <sbenf999@outlook.com>
2023-10-23 18:33:44 +0100

    updated main program, and added testing and view high score functionality

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
 import random
+import datetime
 
 #MAIN_MENU=======================================================
 def get_input(string: str, valid_options: list) -> str:
@@ -10,6 +11,10 @@ def get_input(string: str, valid_options: list) -> str:
 
             if user_input in valid_options:
                 return user_input
+
+            else:
+                print("Input not in valid choices. Re-enter choice: ")
+                user_input = input(string)
             
         except ValueError:
             print(f"{user_input} is not an integer. Re-enter choice: ")
@@ -17,9 +22,9 @@ def get_input(string: str, valid_options: list) -> str:
         
         
 #FILE_HANDLING===================================================
-def writeToFile(filename, data):
+def writeToFile(filename, data, mode="a"):
     try:
-        file = open(filename, "a")
+        file = open(filename, mode)
         for sub_array in data:
             tmp = ""
             for index, element in enumerate(sub_array):
@@ -53,15 +58,59 @@ def openFile(filename, mode="r", split_=","):
             print(f"Could not open {filename}. Error: {e}")
             filename = input("Re-enter filename: ")
 
-def test(filename):
+
+#TESTING-FUNCTION====================================================
+def test_knowledge(filename):
     test_file = openFile(filename)
-    random_keyword = test_file[random.randint(0, len(test_file-1))][0]
+
+    score = 0
+    questions_asked = 0
+
+    while questions_asked != len(test_file):
+        # definition_or_keyword = random.randint(0, 1)
+        definition_or_keyword = 0
+
+        if definition_or_keyword == 0:
+            choices = []
+            chosen = []
+            already = []
+            for i in range(4):
+                random_ = random.randint(0, len(test_file)-1)
+                already.append(random_)
+                while random_ in already:
+                    random_ = random.randint(0, len(test_file)-1)
+                already.append(random_)
+
+                
+
+                choices.append([(test_file[random_][1]).strip("\n"), random_])
+
+            indexes = []
+            for sub_array in choices:
+                indexes.append(sub_array[1])
+
+            rand_index = random.choice(indexes)
+            question_number = questions_asked + 1
+
+            print(f"\n{question_number}) The random keyword is '{test_file[rand_index][0]}'. What is the corresponding defintion?")
+            printList(choices)
+            choice = get_input("> ", [0, 1, 2, 3])
+
+            if (test_file[rand_index][1]).strip("\n") == (choices[choice][0]):
+                print("Correct answer. Score += 1")
+                score += 1
+                questions_asked += 1
+
+            else:
+                print("Incorrect answer.")
+                questions_asked += 1
+
+    username = input(f"You reached a score of: {score}. Enter a username: ")
+    current_time = datetime.datetime.now()
+    writeToFile("High_Scores.txt", [[str(current_time)[:10], username+" - "+str(score)]], "w+")
     
-    return result
 
-def view_scoreboard():
-    pass
-
+#RETURN_OUTPUT_FUNC======================================================
 def return_output(given_list):
     lens = []
      
@@ -81,14 +130,19 @@ def return_output(given_list):
         info = f'{dots}: '.join(output2)
 
         print(f"{info}", end="") 
-        
 
-if __name__ == "__main__":
-    print(("1) Display,\n2) Edit, \n3) Exit"))
-    choice = get_input("> ", [1, 2, 3])
+#PRINT_2D_LIST==========================================================
+def printList(list_):
+    for index, element in enumerate(list_):
+        print(f"{index}){element[0]}")
     
-    while choice != 3:
-        fileNAME = input("Filename: ")
+
+#MAIN_MENU==============================================================
+def main(fileNAME):
+    print(("1) Display,\n2) Edit, \n3) Test, \n4) View Scoreboard \n5) Exit"))
+    choice = get_input("> ", [1, 2, 3, 4, 5])
+
+    while choice != 5:  
         if choice == 1:
             print()
             return_output(openFile(fileNAME))
@@ -101,9 +155,17 @@ if __name__ == "__main__":
             output.append(sub_array)
             
             writeToFile(fileNAME, output)
+
+        elif choice == 3:
+            test_knowledge(fileNAME)
+
+        elif choice == 4:
+            print()
+            return_output(openFile("High_Scores.txt"))
         
-        choice = get_input("\n> ", [1, 2, 3])
-            
-            
+        choice = get_input("\n> ", [1, 2, 3, 4, 5])
+    
     print("Exited program")
     
+
+main(input("Filename: "))
